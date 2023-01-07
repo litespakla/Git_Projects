1c2d2be546c2519723a7ae21a7e5ab36a2b288f2
litespakla <sebas.9624@gmail.com>
2023-01-07 11:22:32 -0600

    Minor corrections

--- Project_Euler_Solutions/p3.py
+++ Project_Euler_Solutions/p3.py
@@ -7,11 +7,11 @@ What is the largest prime factor of the number 600851475143 ?
 #Return largest prime factor of x
 def largest_prime(x):
     i=2
-    while i**2<x:
+    while i**2<=x:
         if x%i==0:
             x=int(x/i)
         else:
             i+=1
     return x
 
-print(largest_prime(20))
+print(largest_prime(600851475143))
