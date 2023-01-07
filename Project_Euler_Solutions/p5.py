f232fd7f259e32ad133039509db4d32995c97390
litespakla <sebas.9624@gmail.com>
2023-01-07 11:22:05 -0600

    Solution to problem 5

--- Project_Euler_Solutions/p5.py
+++ Project_Euler_Solutions/p5.py
@@ -5,3 +5,25 @@ of the numbers from 1 to 10 without any remainder.
 What is the smallest positive number that is evenly
 divisible by all of the numbers from 1 to 20?
 '''
+
+import numpy as np
+
+#Greatest common divisor
+def gcd(a, b):
+    while b != 0:
+        a, b = b, a % b
+    return a
+
+#Smallest common multiple of 2 numbers
+def scm(a, b):
+    return int(a*b/gcd(a,b))
+
+#Returns smallest common multiple of first n numbers
+def common_multiple(n):
+    solution=1
+    for i in range(n):
+        print(solution, i+1)
+        solution=scm(solution, i+1)
+    return solution
+
+print(common_multiple(20))
